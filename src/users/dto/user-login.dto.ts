28f7895b7abc67e74c1328876c778cb512ece87b
Pavlo Petrenko <agronom81@gmail.com>
2023-06-28 08:02:20 +0300

    user login process

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,5 +1,9 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserLoginDto {
+    @IsEmail({}, { message: 'Wrong email or password' })
     email!: string;
+
+    @IsString()
     password!: string;
-    name!: string;
 }
